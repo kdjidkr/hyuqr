d6a544e01b51b1ae31e9ff4afafe46244c71c73e
kdjidkr <kcwidrk@naver.com>
2026-05-10 19:56:39 +0900

    feat: integrate PostHog analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+if (import.meta.env.VITE_POSTHOG_KEY) {
+  posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+    api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com',
+  })
+}
 
 const kakaoKey = import.meta.env.VITE_KAKAO_JS_KEY;
 if (!kakaoKey) {
@@ -35,6 +43,8 @@ if ('serviceWorker' in navigator) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
